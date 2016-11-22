e715719bcde0347c393f53adf8d0cdc9021ae118
Aleksandr Kulek <kulek@kulek-lx.nixsolutions.com>
2016-11-22 19:02:34 +0200

    initial

---
+++ src/test/java/com/nix/config/AppTestConfig.java
@@ -0,0 +1,123 @@
+package com.nix.config;
+
+import com.nix.dao.RoleDao;
+import com.nix.dao.UserDao;
+import com.nix.dao.hiber.HRoleDao;
+import com.nix.dao.hiber.HUserDao;
+import com.nix.service.RoleService;
+import com.nix.service.UserService;
+import com.nix.service.impl.UserServiceImpl;
+import net.tanesha.recaptcha.ReCaptchaImpl;
+import org.hibernate.SessionFactory;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBuilder;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+import java.util.Arrays;
+import java.util.Properties;
+
+@Configuration
+@PropertySource(value = {"classpath:app-test.properties"})
+@EnableTransactionManagement
+public class AppTestConfig {
+
+    @Autowired
+    private Environment env;
+
+    @Bean
+    public RoleDao roleDao() {
+        return new HRoleDao(sessionFactory());
+    }
+
+    @Bean
+    public UserDao userDao() {
+        return new HUserDao(sessionFactory());
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        return new EmbeddedDatabaseBuilder()
+                .setType(EmbeddedDatabaseType.H2)
+                .addScript("dbunit-data/schema.sql")
+                .ignoreFailedDrops(true)
+                .build();
+    }
+
+    @Bean
+    public SessionFactory sessionFactory() {
+        return new LocalSessionFactoryBuilder(dataSource())
+                .scanPackages("com.nix.model")
+                .addProperties(hibernateProperties())
+                .buildSessionFactory();
+    }
+
+    @Bean
+    public HibernateTransactionManager txManager() {
+        HibernateTransactionManager txManager = new HibernateTransactionManager();
+        txManager.setSessionFactory(sessionFactory());
+        return txManager;
+    }
+
+    private Properties hibernateProperties() {
+        return new Properties() {
+            {
+                setProperty("hibernate.cache.use_second_level_cache",
+                        env.getProperty("hibernate.cache.use_second_level_cache"));
+                setProperty("hibernate.cache.use_query_cache",
+                        env.getProperty("hibernate.cache.use_query_cache"));
+                setProperty("hibernate.dialect", env.getProperty("hibernate.dialect"));
+                setProperty("hibernate.max_fetch_depth",
+                        env.getProperty("hibernate.max_fetch_depth"));
+                setProperty("show_sql", env.getProperty("hibernate.show_sql"));
+                setProperty("format_sql", env.getProperty("hibernate.format_sql"));
+                setProperty("hibernate.fetch_size", env.getProperty("hibernate.fetch_size"));
+                setProperty("hibernate.batch_size", env.getProperty("hibernate.batch_size"));
+                setProperty("javax.persistence.validation.mode", "none");
+
+            }
+        };
+    }
+
+    @Bean
+    public UserService userService() {
+        return Mockito.mock(UserServiceImpl.class);
+    }
+
+    @Bean
+    public RoleService roleService() {
+        return Mockito.mock(RoleService.class);
+    }
+
+    @Bean
+    public ReCaptchaImpl reCaptcha() {
+        return Mockito.mock(ReCaptchaImpl.class);
+    }
+
+//    @Bean
+//    public UserDetailsService userDetailsService(){
+//        return Mockito.mock(UserDetailsServiceImpl.class);
+//    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        GrantedAuthority authority = new SimpleGrantedAuthority("ADMIN");
+        UserDetails userDetails = (UserDetails) new User("admin", "pass", Arrays.asList(authority));
+
+        return new InMemoryUserDetailsManager(Arrays.asList(userDetails));
+    }
+}
---
+++ src/test/java/com/nix/controller/SecurityTest.java
@@ -0,0 +1,109 @@
+package com.nix.controller;
+
+import com.nix.config.AppTestConfig;
+import com.nix.config.SecurityConfig;
+import com.nix.config.WebAppConfig;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcConfigurer;
+import org.springframework.web.context.WebApplicationContext;
+
+import javax.servlet.Filter;
+
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.formLogin;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
+import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.authenticated;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrlPattern;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@ContextConfiguration(
+        classes = {AppTestConfig.class,
+                WebAppConfig.class,
+                SecurityConfig.class})
+@WebAppConfiguration
+public class SecurityTest {
+
+    @Autowired
+    private WebApplicationContext wac;
+    @Autowired
+    private Filter springSecurityFilterChain;
+
+    @Autowired
+    @Mock
+    private UserDetailsService userDetailsServiceMock;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setUp() throws Exception {
+        mockMvc = MockMvcBuilders
+                .webAppContextSetup(wac)
+                .apply(springSecurity(springSecurityFilterChain))
+                .build();
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test(timeout = 2000L)
+    public void getAdminPageNotAuthorize() throws Exception {
+
+        mockMvc.perform(get("/admin/users"))
+                .andDo(print())
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrlPattern("**/login"));
+
+    }
+
+    @Test(timeout = 2000L)
+    @WithMockUser(roles={"ADMIN"})
+    public void userPerformLogin() throws Exception {
+
+//        when(userDetailsServiceMock.loadUserByUsername(anyString()))
+//                .thenReturn(any(UserDetails.class));
+
+        mockMvc
+                .perform(post("/login").with(httpBasic("user","pass")))
+//                .perform(formLogin("/login").user("admin").password("pass"))
+                .andDo(print());
+
+//        verify(userDetailsServiceMock, times(1)).loadUserByUsername(anyString());
+
+    }
+
+
+    @Test
+    public void requiresAuthentication() throws Exception {
+        mockMvc
+                .perform(get("/admin/users"))
+                .andExpect(status().isFound());
+    }
+
+    @Test
+    public void authenticationSuccess() throws Exception {
+        mockMvc
+                .perform(formLogin().user("login", "testUser_1").password("pwd", "testUser_1"))
+                .andExpect(status().isFound())
+                .andExpect(redirectedUrl("/"))
+                .andExpect(authenticated().withUsername("user"));
+    }
+}
